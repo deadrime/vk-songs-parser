9634f08d37fedafc56d2c7e8cce8f8fdec345387
Jenya Trofimov <deadrime@yandex.ru>
2019-02-05 16:07:47 +0300

    Save playlist as .m3u

--- parser.js
+++ parser.js
@@ -24,7 +24,9 @@ export default class VKParser {
     this.cookies = []
     this.browser = null
     this.page = null
+    this.userId = null
   }
+
   async init() {
     this.browser = await puppeteer.launch(this.options)
     this.page = await this.browser.newPage()
@@ -33,6 +35,7 @@ export default class VKParser {
     this.page.on('request', requestInterceptor)
     await this.login()
   }
+
   async login() {
     const usernameSelector = `input[name="email"]`
     const passwordSelector = `input[type="password"]`
@@ -45,15 +48,20 @@ export default class VKParser {
     await this.page.keyboard.type(this.options.password)
     await this.page.click(buttonSelector)
     this.cookies = await this.page.cookies()
+      .then(data => data
+        .filter(({ name }) => ['remixstid', 'remixgp', 'remixsid'].includes(name))
+        .map(({ name, value }) => `${name}=${value};`)
+        .join(' '))
+    await this.page.goto('https://m.vk.com/')
+    await this.page.waitFor(100)
+    const content = await this.page.content()
+    this.userId = cheerio.load(content)('a.op_owner').attr('href').split('/id')[1]
   }
+
   async parseUserSongs(id) {
     signale.time('Парсинг')
     let allSongs = []
     let offset = 0
-    let Cookie = this.cookies
-      .filter(({ name }) => ['remixstid', 'remixgp', 'remixsid'].includes(name))
-      .map(({ name, value }) => `${name}=${value};`)
-      .join(' ')
     while (1) {
       let page = await rp({
         url: `https://m.vk.com/audios${id}`,
@@ -62,10 +70,10 @@ export default class VKParser {
           offset
         },
         headers: {
-          Cookie: Cookie
+          Cookie: this.cookies
         }
       })
-      let songs = await this.parseSongsFromPage(cheerio.load(page), id)
+      let songs = await this.parseSongsFromPage(cheerio.load(page), this.userId)
       if (!songs) {
         signale.timeEnd('Парсинг')
         return allSongs
@@ -74,10 +82,12 @@ export default class VKParser {
       offset += songs.length
     }
   }
+
   parseSongsFromPage($, vkId) {
     let audios = $('#au_search_items').find('.audio_item')
     let result = []
-    audios.each((i, audio) => {
+
+    audios.each((_, audio) => {
       const artist = formatName($(audio).find('.ai_artist').text())
       const title = formatName($(audio).find('.ai_title').text())
       const duration = $(audio).find('.ai_dur').data('dur')
@@ -95,10 +105,12 @@ export default class VKParser {
     if (result.length === 0) return null
     return result
   }
+
   async downloadSongs(songs) {
     const progressBar = new Signale({interactive: true, scope: 'downloading'})
     let downloadCount = 0
-    await Promise.all(songs.map(async ({artist, title, mp3}) => {
+    const writer = require('m3u').extendedWriter()
+    await Promise.all(songs.map(async ({artist, title, mp3, duration}) => {
       const options = {
         url: mp3,
         encoding: null
@@ -106,12 +118,17 @@ export default class VKParser {
       const result = await rp.get(options)
       progressBar.complete(`[${++downloadCount}/${songs.length}] ${artist} - ${title}`)
       const buffer = Buffer.from(result, 'utf8')
-      fs.writeFileSync(`${__dirname}/songs/${artist} - ${title}.mp3`, buffer)
+      const filePath = `${__dirname}/songs/${artist} - ${title}.mp3`
+      fs.writeFileSync(filePath, buffer)
+      writer.file(filePath, duration, `${artist} - ${title}`)
     }))
+
+    fs.writeFileSync(`${__dirname}/songs/playlist.m3u`, writer.toString())
     progressBar.success(`[${songs.length}/${songs.length}]`)
     return songs
   }
-  saveToJSON(songs) {
+
+  async saveToJSON(songs) {
     fs.writeFileSync('playlist.json', JSON.stringify(songs, null, 4))
     signale.complete('playlist.json')
     return songs
@@ -125,7 +142,8 @@ const parser = new VKParser({
 
 parser.init()
   .then(() => parser.parseUserSongs(config.page))
-  .then(songs => {
-    parser.saveToJSON(songs)
-    parser.downloadSongs(songs)
+  .then(parser.saveToJSON)
+  .then(parser.downloadSongs)
+  .then(() => {
+    process.exit()
   })
